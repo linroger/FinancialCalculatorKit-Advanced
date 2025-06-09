651cebe9ba2ce57c888e53efc755f2510e7cdbf2
Roger Lin <linroger023@uchicago.edu>
2025-06-08 21:13:47 -0700

    Initial Commit

---
+++ FinancialCalculatorKitTests/FinancialCalculatorKitTests.swift
@@ -0,0 +1,16 @@
+//
+//  FinancialCalculatorKitTests.swift
+//  FinancialCalculatorKitTests
+//
+//  Created by Roger Lin on 6/8/25.
+//
+
+import Testing
+
+struct FinancialCalculatorKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
